c32c8d6447a3818f36ca3153994411f17098d524
sekgothetshepang <sekgothetshepang30@gmail.com>
2024-10-22 03:20:50 +0200

    Update subscriptionroutes.js

--- routes/subscriptionroutes.js
+++ routes/subscriptionroutes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+
+// Import the subscription controller
 const subscriptionController = require('../controllers/subscriptionController');
 
-router.post('/subscribe', subscriptionController.subscribe);
-router.get('/subscriptions', subscriptionController.getSubscriptions);
+// POST route to create a subscription
+router.post('/', subscriptionController.createSubscription); // This will handle /api/subscribe
 
+// GET route to retrieve subscriptions
+router.get('/', subscriptionController.getSubscriptions);  // This will handle /api/subscribe (or /api/subscriptions, depending on your design)
+
+// Export the router
 module.exports = router;
